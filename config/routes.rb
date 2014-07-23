0632abff87965153698916da9f0aae59688f51ae
paul713 <padizon13@gmail.com>
2014-07-23 21:23:53 +0800

    Created Homepage Layout
    Signed-off-by: paul713 <padizon13@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'static_pages/help'
-  get 'static_pages/contact'
+
+  get 'home', to: 'static_pages#home'
+  get 'contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
